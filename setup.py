a698c463c5cf56e035cffea07b18105e82bea304
tongshiwei <tongsw@mail.ustc.edu.cn>
2019-07-01 19:27:31 +0800

    添加安装脚本

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# create by tongshiwei on 2019/6/25
+
+from setuptools import setup
+
+setup(
+    name='EduSim',
+    version='0.0.1',
+    install_requires=[
+        'gym',
+        'longling>=1.0.0',
+        'tqdm',
+        'networkx',
+        'numpy'
+    ]  # And any other dependencies foo needs
+)
