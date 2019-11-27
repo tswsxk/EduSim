27bb6bcdba07af00cd3321369a9e7de0d2d92709
tswsxk <tongsw@mail.ustc.edu.cn>
2019-11-27 10:53:27 +0800

    test for KSS

---
+++ tests/test_KSS/conftest.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+# 2019/11/27 @ tongshiwei
+
+import pytest
+from EduSim.Envs.KSS import KSS
+
+
+@pytest.fixture(scope="module")
+def env():
+    return KSS(learner_num=5)
+
+
+@pytest.fixture(scope="module")
+def learner(env):
+    yield env.begin_episode()
+    env.end_episode()
+
+
+@pytest.fixture(scope="module")
+def q_bank(env):
+    return env.q_bank
+
+
+@pytest.fixture(scope="module")
+def tester(env):
+    return env.tester
