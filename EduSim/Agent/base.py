a7a04b6915e359c246c79d7c3cf9fd4a090f3599
tswsxk <tongsw@mail.ustc.edu.cn>
2019-11-28 14:04:47 +0800

    key check

---
+++ EduSim/Agent/base.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# 2019/11/28 @ tongshiwei
+
+return_key_agent_summary_episode = {
+    "learner_id", "learning_history", "exercise_history",
+    "path", "interactions", "episode_reward"
+}
