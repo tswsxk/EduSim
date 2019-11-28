abbde5d668e6552c628118f76be6b554aa845c14
tswsxk <tongsw@mail.ustc.edu.cn>
2019-11-28 15:44:17 +0800

    partial fix

--- tests/test_KSS/test_learner.py
+++ tests/test_KSS/test_learner.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 # 2019/11/27 @ tongshiwei
+# potential bugs
 
 
-def test_learner(learner):
-    initial_ability = learner.test(0)
+def test_learner(tester, learner):
+    tester.begin_episode(learner)
+    initial_ability = tester.exam(binary=False)
     assert isinstance(initial_ability, (int, float))
-    assert initial_ability == learner.test(0)
+    assert initial_ability == tester.exam(binary=False)
     for i in learner.target:
         learner.learn(i)
-    assert initial_ability != learner.test(0)
+    assert initial_ability != tester.exam(binary=False)
