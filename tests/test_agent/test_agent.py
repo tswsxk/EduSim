5160d81d2bb5b37639fcf87243477969f54bbbf4
tswsxk <tongsw@mail.ustc.edu.cn>
2019-11-28 15:49:05 +0800

    fix bug

--- tests/test_agent/test_agent.py
+++ tests/test_agent/test_agent.py
@@ -6,13 +6,14 @@ from EduSim.Agent.base import return_key_agent_summary_episode
 
 def test_agent(random_agent, learner):
     agent = random_agent
-    agent.begin_episode(learner)
-    agent.step_reward()
-    learning_item = agent.step()
-    agent.observe(learning_item, 0)
-    learning_item = agent.step()
-    assert isinstance(learning_item, int)
-    agent.n_step(10)
-    agent.episode_reward(10)
-    assert set(return_key_agent_summary_episode) == set(agent.end_episode())
-    agent.tune()
+    with agent.episode(learner):
+        agent.step_reward()
+        learning_item = agent.step()
+        agent.observe(learning_item, 0)
+        learning_item = agent.step()
+        assert isinstance(learning_item, int)
+        agent.n_step(10)
+        agent.episode_reward(10)
+        assert isinstance(agent.path, list)
+        assert set(return_key_agent_summary_episode) == set(agent.summary_episode())
+        agent.tune()
