98410dba389080f9f4955b0a7e87794d68ff3196
tswsxk <tongsw@mail.ustc.edu.cn>
2019-11-28 15:00:47 +0800

    update dependency

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
                 'provide some offline simulators for training and testing recommender systems of education.',
     install_requires=[
         'gym',
-        'longling>=1.0.0',
+        'longling>=1.3.0',
         'tqdm',
         'networkx',
         'numpy'
