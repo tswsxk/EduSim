607ef948c09edd192ef2b98b716a000af0176737
Sherlcok <tongsw@mail.ustc.edu.cn>
2020-04-21 21:16:20 +0800

    add examples

--- EduSim/utils/irt.py
+++ EduSim/utils/irt.py
@@ -7,4 +7,12 @@ import math
 
 
 def irt(ability, difficulty, discrimination=5, c=0.25):
+    """
+    Examples
+    --------
+    >>> round(irt(3, 1), 2)
+    1.0
+    >>> round(irt(1, 5), 3)
+    0.25
+    """
     return c + (1 - c) / (1 + math.exp(-1.7 * discrimination * (ability - difficulty)))
